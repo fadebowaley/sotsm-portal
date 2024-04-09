7d62d7e37cb5ff0f1e12b52ea9a7783c1b711a37
Ademola Adebowale <fadebowaley@gmail.com>
2024-04-09 18:07:33 +0100

    update of js submission

---
+++ public/assets/js/dataSubmit.js
@@ -0,0 +1,98 @@
+"strict"
+
+// Get form elements for personal Information
+const title = document.getElementById("title").value;
+const firstname = document.getElementById("firstname").value;
+const lastname = document.getElementById("lastname").value;
+const otherName = document.getElementById("otherName").value;
+const email = document.getElementById("email").value;
+const phone = document.getElementById("phone").value;
+const genderMale = document.getElementById("male").checked;
+const genderFemale = document.getElementById("female").checked;
+const dateOfBirth = document.getElementById("dateOfBirth").value;
+const highestQualification = document.getElementById(
+  "highestQualification"
+).value;
+const professionalQualification = document.getElementById(
+  "professionalQualification"
+).value;
+const maritalStatus = document.getElementById("maritalStatus").value;
+const stateOfOrigin = document.getElementById("stateSelect4").value;
+const lgaOfOrigin = document.getElementById("lgaSelect4").value;
+const homeTown = document.getElementById("homeTown").value;
+const spouseName = document.getElementById("spouseName").value;
+const spousePhoneNumber = document.getElementById("spousePhoneNumber").value;
+const spouseDateOfBirth = document.getElementById("spouseDateOfBirth").value;
+const nextOfKinName = document.getElementById("nextOfKinName").value;
+const nextOfKinPhoneNumber = document.getElementById(
+  "nextOfKinPhoneNumber"
+).value;
+const nextOfKinRelationship = document.getElementById(
+  "nextOfKinRelationship"
+).value;
+const residentialAddress = document.getElementById("residentialAddress").value;
+const stateOfResidence = document.getElementById("stateSelect1").value;
+const lgaOfResidence = document.getElementById("lgaSelect1").value;
+const employmentCategory = document.getElementById("emp-category").value;
+const occupation = document.getElementById("occupation").value;
+
+
+//Get all element of the second form
+// Get all elements for the second form
+const yearBornAgain = document.getElementById('yearBornAgain').value;
+const waterBaptized = document.getElementById('waterBaptized').value;
+const holyghostBaptism = document.getElementById('holyghostBaptism').value;
+const joinedSotsm = document.getElementById('joinedSotsm').value;
+const becameWorker = document.getElementById('becameWorker').value;
+const becameMinister = document.getElementById('becameMinister').value;
+const ordainedDcns = document.getElementById('ordainedDcns').value;
+const becamePastor = document.getElementById('becamePastor').value;
+const becameSnrPastor = document.getElementById('becameSnrPastor').value;
+const ordainedElder = document.getElementById('ordainedElder').value;
+const ordainedBishop = document.getElementById('ordainedBishop').value;
+const lastOrdinationDate = document.getElementById('lastOrdinationDate').value;
+const IBSCOMS = document.getElementById('IBSCOMS').value;
+const WOOCOME = document.getElementById('WOOCOME').value;
+const ILS = document.getElementById('ILS').value;
+const NGBTI = document.getElementById('NGBTI').value;
+const placeOfAssignment = document.getElementById('placeOfAssignment').value;
+const nameOfzone = document.getElementById('nameOfzone').value;
+const nameOfDiocese = document.getElementById('nameOfDiocese').value;
+const nameOfDivision = document.getElementById('nameOfDivision').value;
+const stateSelect2 = document.getElementById('stateSelect2').value;
+const lgaSelect2 = document.getElementById('lgaSelect2').value;
+const placeOfAssignmentAddress = document.getElementById('placeOfAssignmentAddress').value;
+const countryChurch = document.getElementById('countryChurch').value;
+const jobTitle = document.getElementById('jobTitle').value;
+const yrEmployed = document.getElementById('yrEmployed').value;
+const unitInDepartment = document.getElementById('unitInDepartment').value;
+const gradeLevel = document.getElementById('gradeLevel').value;
+const stepLevel = document.getElementById('stepLevel').value;
+const deptAdress = document.getElementById('deptAdress').value;
+const stateSelect3 = document.getElementById('stateSelect3').value;
+const lgaSelect3 = document.getElementById('lgaSelect3').value;
+const countryOrg = document.getElementById('countryOrg').value;
+const ispastor_parish = document.getElementById('ispastor_parish').value;
+const careerPastorOffice = document.getElementById('careerPastorOffice').value;
+
+// Now you have all the values of the elements for the second form
+const dateEstablished = document.getElementById('dateEstablished').value;
+const churchProperty = document.getElementById('churchProperty').value;
+const propertyEstimatedValue = document.getElementById('propertyEstimatedValue').value;
+const clcBuilding = document.getElementById('clcBuilding').value;
+const leaseType = document.getElementById('leaseType').value;
+const avgadult = document.getElementById('avgadult').value;
+const avgyouth = document.getElementById('avgyouth').value;
+const avgchildren = document.getElementById('avgchildren').value;
+const total = document.getElementById('total').value;
+const totalWorkers = document.getElementById('totalWorkers').value;
+const workersInTraining = document.getElementById('workersInTraining').value;
+const unordainedLeaders = document.getElementById('unordainedLeaders').value;
+const noofMinisters = document.getElementById('noofMinisters').value;
+const noofdcns = document.getElementById('noofdcns').value;
+const noofpastor = document.getElementById('noofpastor').value;
+const noofsnrpastor = document.getElementById('noofsnrpastor').value;
+const noofelder = document.getElementById('noofelder').value;
+const noofbishops = document.getElementById('noofbishops').value;
+
+// Now you have all the values of the elements for the third form
